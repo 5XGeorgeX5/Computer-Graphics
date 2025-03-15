28d48afee6b820a7d5d8c13aecad227742b98590
George Raafat <georgexraafat@gmail.com>
2025-03-15 15:22:42 +0200

    Add circle drawing functionality and .gitignore for executable files

---
+++ Assignment#1/circle.cpp
@@ -0,0 +1,110 @@
+//////////////////////////////
+// George Raafat - 20220097 //
+//////////////////////////////
+
+#define UNICODE
+#define _UNICODE
+#include <Windows.h>
+#include <cmath>
+
+int Round(double x)
+{
+    return (int)(x + 0.5);
+}
+
+void DrawCirclePoints(HDC hdc, int xc, int yc, int x, int y, COLORREF c)
+{
+    SetPixel(hdc, xc + x, yc + y, c);
+    SetPixel(hdc, xc - x, yc + y, c);
+    SetPixel(hdc, xc + x, yc - y, c);
+    SetPixel(hdc, xc - x, yc - y, c);
+
+    SetPixel(hdc, xc + y, yc + x, c);
+    SetPixel(hdc, xc - y, yc + x, c);
+    SetPixel(hdc, xc + y, yc - x, c);
+    SetPixel(hdc, xc - y, yc - x, c);
+}
+
+void DrawCircleBres(HDC hdc, int xc, int yc, int R, COLORREF c)
+{
+    int x = 0, y = R;
+    int d = 1 - R;
+    int d1 = 3, d2 = 5 - 2 * R;
+    DrawCirclePoints(hdc, xc, yc, x, y, c);
+    while (x < y)
+    {
+        if (d < 0)
+        {
+            d += d1;
+            d2 += 2;
+        }
+        else
+        {
+            d += d2;
+            d2 += 4;
+            y--;
+        }
+        d1 += 2;
+        x++;
+        DrawCirclePoints(hdc, xc, yc, x, y, c);
+    }
+}
+
+LRESULT WndProc(HWND hwnd, UINT m, WPARAM wp, LPARAM lp)
+{
+    static int xc, yc; // center point
+    HDC hdc;
+    switch (m)
+    {
+    case WM_LBUTTONDOWN:
+        xc = LOWORD(lp);
+        yc = HIWORD(lp);
+        break;
+    case WM_LBUTTONUP:
+    {
+        int dx = xc - LOWORD(lp);
+        int dy = yc - HIWORD(lp);
+        int R = Round(sqrt(dx * dx + dy * dy)); // radius
+
+        hdc = GetDC(hwnd);
+        DrawCircleBres(hdc, xc, yc, R, RGB(0, 0, 0));
+        ReleaseDC(hwnd, hdc);
+        break;
+    }
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, m, wp, lp);
+    }
+    return 0;
+}
+
+int APIENTRY WinMain(HINSTANCE hi, HINSTANCE pi, LPSTR cmd, int nsh)
+{
+    WNDCLASS wc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+    wc.lpszClassName = L"MyClass";
+    wc.lpszMenuName = NULL;
+    wc.lpfnWndProc = WndProc;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.hInstance = hi;
+    RegisterClass(&wc);
+    HWND hwnd = CreateWindow(L"MyClass", L"Hello World!", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hi, 0);
+    ShowWindow(hwnd, nsh);
+    UpdateWindow(hwnd);
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
