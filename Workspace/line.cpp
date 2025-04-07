28d87918d18c49ee40ac8f972f50bd52d8d3e7a3
George Raafat <georgexraafat@gmail.com>
2025-04-07 12:18:35 +0200

    Remove unused DrawLine function to streamline line drawing implementations

--- Workspace/line.cpp
+++ Workspace/line.cpp
@@ -181,32 +181,6 @@ void DrawLineBresenham(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c)
     }
 }
 
-void DrawLine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color)
-{
-    int dx = abs(x2 - x1), dy = abs(y2 - y1);
-    int sx = (x1 < x2) ? 1 : -1;
-    int sy = (y1 < y2) ? 1 : -1;
-    int err = dx - dy;
-
-    while (true)
-    {
-        SetPixel(hdc, x1, y1, color);
-        if (x1 == x2 && y1 == y2)
-            break;
-        int e2 = 2 * err;
-        if (e2 > -dy)
-        {
-            err -= dy;
-            x1 += sx;
-        }
-        if (e2 < dx)
-        {
-            err += dx;
-            y1 += sy;
-        }
-    }
-}
-
 LRESULT WndProc(HWND hwnd, UINT m, WPARAM wp, LPARAM lp)
 {
     HDC hdc;
